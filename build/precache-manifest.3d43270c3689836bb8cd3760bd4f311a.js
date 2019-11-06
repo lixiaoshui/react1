f4bbce73cad253de59f39891fad4ded035f32213
lixiaoshui <2087751927@qq.com>
2019-11-06 23:03:02 +0800

    lijia

---
+++ build/precache-manifest.3d43270c3689836bb8cd3760bd4f311a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343b8c0d957b7722193c763d55b57d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53785f8c7e704bdbf59c",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "528aa478543327a07d0f",
+    "url": "/static/css/main.86834e99.chunk.css"
+  },
+  {
+    "revision": "53785f8c7e704bdbf59c",
+    "url": "/static/js/2.ea7f778b.chunk.js"
+  },
+  {
+    "revision": "528aa478543327a07d0f",
+    "url": "/static/js/main.fbc863bf.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
